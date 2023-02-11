37821bf971e4915c4a7c828ce7ebc480dad4f1ee
Brooke Kennison <brookelkennison@gmail.com>
2023-02-11 11:09:07 -0500

    blog section

--- front-page.php
+++ front-page.php
@@ -10,6 +10,29 @@
     <div class="pastClients">
         <h6>Clients I have served:</h6>
     </div>
+    <div class="blog">
+        <h2>on the blog</h2>
+        <div class="featured-posts">
+            <?php
+            $args = array(
+                'category' => get_cat_ID('featured')
+            );
+            $latest_posts = get_posts($args);
+            // var_dump($latest_posts);
+            foreach ($latest_posts as $post) {
+            ?>
+                <div class="post">
+                    <?php echo get_the_post_thumbnail($post->ID, 'medium'); ?>
+                    <h3><?php echo apply_filters('the_title', $post->post_title, $post->ID); ?></h3>
+                    <p><a href="<?php echo $post->guid ?>">Learn More -><a></p>
+                </div>
+
+            <?php
+            }
+            ?>
+
+        </div> <?php ?>
+    </div>
 </section>
 
 <?php get_footer(); ?>
